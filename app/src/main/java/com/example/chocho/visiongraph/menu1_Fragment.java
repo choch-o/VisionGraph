4e84fa5b3846adf7ee6cdbc861133f74510b79f1
hscho1226@gmail.com <hscho1226@gmail.com>
2015-01-07 03:20:38 +0900

    Navigation Drawer done.

--- app/src/main/java/com/example/chocho/visiongraph/menu1_Fragment.java
+++ app/src/main/java/com/example/chocho/visiongraph/menu1_Fragment.java
@@ -17,6 +17,5 @@ public class menu1_Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.menu1_layout, container, false);
         return rootview;
-        //hi
     }
 }
