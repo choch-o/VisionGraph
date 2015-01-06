0d9732502e2ca7031a1f1e9094355398e1d160e7
hscho1226@gmail.com <hscho1226@gmail.com>
2015-01-07 03:03:52 +0900

    First commit. Navigation Drawer done.

---
+++ app/src/main/java/com/example/chocho/visiongraph/menu1_Fragment.java
@@ -0,0 +1,21 @@
+package com.example.chocho.visiongraph;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chocho on 2015-01-07.
+ */
+public class menu1_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu1_layout, container, false);
+        return rootview;
+    }
+}
---
+++ app/src/main/java/com/example/chocho/visiongraph/menu2_Fragment.java
@@ -0,0 +1,21 @@
+package com.example.chocho.visiongraph;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by chocho on 2015-01-07.
+ */
+public class menu2_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu2_layout, container, false);
+        return rootview;
+    }
+}
