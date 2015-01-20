7fb13517caf9516f0d2806dfa913985200e25a03
hscho1226@gmail.com <hscho1226@gmail.com>
2015-01-21 01:19:22 +0900

    나의 비전: 텍스트 저장, 디자인 변경

--- app/src/main/java/com/example/chocho/visiongraph/editActivity.java
+++ app/src/main/java/com/example/chocho/visiongraph/editActivity.java
@@ -1,21 +1,36 @@
 package com.example.chocho.visiongraph;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by chocho on 2015-01-19.
  */
 public class editActivity extends Activity {
+    String content;
+    String savedContent;
+    EditText answerText;
+    int num;
+    Intent intent = new Intent();
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.editactivity);
+        num = (int) getIntent().getIntExtra("num", 0);
+        answerText = (EditText) findViewById(R.id.editBox);
+        savedContent = getIntent().getStringExtra("content");
+        answerText.setText(savedContent);
         Button doneBtn = (Button) findViewById(R.id.doneBtn);
         doneBtn.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-
+                content = answerText.getText().toString();
+                intent.putExtra("content", content);
+                intent.putExtra("num", num);
+                setResult(RESULT_OK, intent);
+                finish();
             }
         });
     }
